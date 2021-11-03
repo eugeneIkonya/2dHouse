20ff1af39e68f825b24234a56af7344fccdb7b95
eugene.ikonya@strathmore.edu <eugene.ikonya@strathmore.edu>
2021-11-03 12:51:52 +0300

    initial commit

---
+++ nono/main.cpp
@@ -0,0 +1,26 @@
+#include "polygon.cpp"
+#include "star.cpp"
+#include <iostream>
+#include "line.cpp"
+#include "House.cpp"
+
+
+
+
+int main(int argc, const char ** argv) {
+
+//    line line;
+//    line.drawLine();
+    
+//    Polygon hexagon;
+//    hexagon.drawPolygon();
+    
+//    Star star;
+//    star.drawStar();
+    
+    House house;
+    house.drawHouse();
+    
+    return 0;
+    
+}
